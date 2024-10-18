513202214062a0a55646033beab9bfa241ea1657
spidercupcake <xmze11690911300@gmail.com>
2024-10-18 15:30:15 +0500

    install and configure webpack.
    -   `npm install --save-dev webpack webpack-cli webpack-dev-server html-webpack-plugin clean-webpack-plugin@latest
    `

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js", // or "./src/index.jsx" if you're using JSX
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true, // Cleans the output directory before each build (if using Webpack 5)
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // Optional: cleans output directory
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 3000,
+    hot: true,
+  },
+};
